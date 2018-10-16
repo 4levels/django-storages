4a8dc8e63e5452bb3896dc809e3388fac260f9f9
4levels <4levels@gmail.com>
2018-10-16 13:57:17 +0200

    Added DropBoxTeamStorage class to enable Dropbox Team support

--- storages/backends/dropbox.py
+++ storages/backends/dropbox.py
@@ -139,3 +139,47 @@ class DropBoxStorage(Storage):
                     content.read(self.CHUNK_SIZE), cursor
                 )
                 cursor.offset = content.tell()
+
+
+@deconstructible
+class DropBoxTeamStorage(DropBoxStorage):
+    """
+    Dropbox Team Storage class for using with Team accounts
+    When working with Dropbox Teams (part of the Business API), the API is
+    different: when using the team token, you cannot access the folders without
+    becoming a user first.  On top of that, without the Team folder namespace,
+    you can only access the users personal folder, not the team folder.
+    You need to use the header method to set the root path.  You also need
+    the id of an admin user and your app needs to have the
+    "Team member file access" permission for this to work
+    Caveat: If you use the namespace id as part of the path (ns:xxxx/path)
+    instead of using the Dropbox-API-Path-Root header, no path info is returned
+    in the file_list_folder responses - unexpected to say the least
+    """
+
+    def __init__(self, oauth2_access_token=None, root_path=None, headers=None,
+                 team_namespace=None, team_admin=None):
+        oauth2_access_token = oauth2_access_token or \
+            setting('DROPBOX_OAUTH2_TOKEN')
+        team_namespace = team_namespace or setting('DROPBOX_TEAM_NAMESPACE')
+        team_admin = team_admin or setting('DROPBOX_TEAM_ADMIN')
+        self.root_path = root_path or setting('DROPBOX_ROOT_PATH', '/')
+        if oauth2_access_token is None:
+            raise ImproperlyConfigured("You must configure a token auth at"
+                                       "'settings.DROPBOX_OAUTH2_TOKEN'.")
+        if team_namespace is None:
+            raise ImproperlyConfigured("You must configure a namespace at"
+                                       "'settings.DROPBOX_TEAM_NAMESPACE'.")
+        if team_admin is None:
+            raise ImproperlyConfigured("You must configure an admin at"
+                                       "'settings.DROPBOX_TEAM_ADMIN'.")
+
+        # add Dropbox-API-Path-Root header using the namespace syntax
+        self.client = DropboxTeam(
+            oauth2_access_token,
+            headers={
+                'Dropbox-API-Path-Root':
+                    '{".tag": "namespace_id", "namespace_id": "%s"}' %
+                    team_namespace
+            }
+        ).as_admin("dbmid:%s" % team_admin)  # and become a user
